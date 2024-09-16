3e99fd3caf489312d070c732c891bf3fcf50ed1b
shajjar-emumba <muhammad.shajjar@emumba.com>
2024-09-16 16:02:06 +0500

    config(firebase): add firebase configurations for firebase auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,5 @@
+import { FirebaseAuth } from './../../node_modules/@firebase/auth-types/index.d';
+import { Auth, getAuth, provideAuth } from '@angular/fire/auth';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -5,6 +7,8 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +18,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
     provideStore(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
 };
